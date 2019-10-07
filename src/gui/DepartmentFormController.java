f27d21a7e67b871e23ae2c201cf145e9fbe3ac92
iararmsantos <iararmsantos@gmail.com>
2019-10-06 20:28:15 -0400

    DepartmentForm design

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,23 @@
+
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Iara Santos
+ */
+public class DepartmentFormController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
