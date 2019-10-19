fcb6ecab5eef9882bf418cd449809626505e326b
iararmsantos <iararmsantos@gmail.com>
2019-10-19 08:52:36 -0400

    Passing a Department object to DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,6 +1,7 @@
 
 package gui;
 
+import gui.util.Constraints;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -9,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 /**
  * FXML Controller class
@@ -16,6 +18,8 @@ import javafx.scene.control.TextField;
  * @author Iara Santos
  */
 public class DepartmentFormController implements Initializable {
+    //create dependy to Department
+    private Department entity;
 
     @FXML
     private TextField txtId;
@@ -33,8 +37,13 @@ public class DepartmentFormController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        initializeNodes();
+    } 
+    
+    private void initializeNodes(){
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
 
     @FXML
     private void onBtSaveAction(ActionEvent event) {
@@ -45,5 +54,18 @@ public class DepartmentFormController implements Initializable {
     private void onBtCancelAction(ActionEvent event) {
         System.out.println("onBtCancelAction");
     }
+    public void setDepartment(Department entity){
+        this.entity = entity;
+    }
     
+    //method to populate form
+    public void updateFormDate(){
+        //defensive action
+        if(entity == null){
+            throw new IllegalStateException("Entity was null");
+        }
+        //convert int to int to string
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
+    }
 }
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -56,7 +56,10 @@ public class DepartmentListController implements Initializable {
     @FXML
     private void onBtNewAction(ActionEvent event) {
         Stage parentStage = Utils.currentStage(event);
-        createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+        
+        Department obj = new Department();      
+        
+        createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
     }
     
     //inversao de controle
@@ -86,10 +89,15 @@ public class DepartmentListController implements Initializable {
         tableViewDepartment.setItems(obsList);
     }
     
-    private void createDialogForm(String absoluteName, Stage parentStage) {
+    private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
+            
+            DepartmentFormController controller = loader.getController();
+            controller.setDepartment(obj);
+            //load form
+            controller.updateFormDate();
 
             Stage dialogStage = new Stage();
             dialogStage.setTitle("Enter Department data");
