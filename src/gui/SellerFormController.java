b88f23e5f4d8c1851f0f1379716a779b61512054
iararmsantos <iararmsantos@gmail.com>
2019-10-21 19:56:38 -0400

    Department ComboBox

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -7,20 +7,32 @@ import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Department;
 import model.entities.Seller;
 import model.exceptions.ValidationException;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 /**
@@ -35,6 +47,9 @@ public class SellerFormController implements Initializable {
     //create depency to SellerService
     private SellerService service;
     
+    //add dependency DepartmentService
+    private DepartmentService departmentService;
+    
     //aceitar inscricao dos elementos que irao receber event
     private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 
@@ -48,6 +63,20 @@ public class SellerFormController implements Initializable {
     private Button btSave;
     @FXML
     private Button btCancel;
+    @FXML
+    private Label labelErrorEmail;
+    @FXML
+    private Label labelErrorBirthDate;
+    @FXML
+    private Label labelErrorBaseSalary;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtBaseSalary;
+    @FXML
+    private DatePicker dpBirthDate;
+    @FXML
+    private ComboBox<Department> comboBoxDepartment;
 
     /**
      * Initializes the controller class.
@@ -59,7 +88,11 @@ public class SellerFormController implements Initializable {
     
     private void initializeNodes(){
         Constraints.setTextFieldInteger(txtId);
-        Constraints.setTextFieldMaxLength(txtName, 30);
+        Constraints.setTextFieldMaxLength(txtName, 70);
+        Constraints.setTextFieldDouble(txtBaseSalary);
+        Constraints.setTextFieldMaxLength(txtEmail, 60);
+        Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+        initializeComboBoxDepartment();
     }
 
     @FXML
@@ -94,12 +127,16 @@ public class SellerFormController implements Initializable {
     private void onBtCancelAction(ActionEvent event) {
         Utils.currentStage(event).close();
     }
+    
+    private ObservableList<Department> obsList;
+    
     public void setSeller(Seller entity){
         this.entity = entity;
     }
     
-    public void setSellerService(SellerService service){
+    public void setServices(SellerService service, DepartmentService departmentService){
         this.service = service;
+        this.departmentService = departmentService;
     }
     
     public void subscribeDataChangeListener(DataChangeListener listener){
@@ -115,7 +152,32 @@ public class SellerFormController implements Initializable {
         //convert int to int to string
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+        txtEmail.setText(entity.getEmail());
+        Locale.setDefault(Locale.US);
+        txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+        
+        if(entity.getBirthDate() != null){
+            dpBirthDate.setValue(LocalDateTime.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()).toLocalDate());
+        }
+        if(entity.getDepartment() == null){
+            comboBoxDepartment.getSelectionModel().selectFirst();
+        }
+        else
+        {
+            comboBoxDepartment.setValue(entity.getDepartment());
+        
+        }
     }
+    
+    public void loadAssociatedObjects(){
+        if(departmentService == null){
+            throw new IllegalStateException("DepartmentService was null");
+        }
+        List<Department> list = departmentService.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        comboBoxDepartment.setItems(obsList);
+    }
+    
     //metodo responsavel por pegar a excessao e lanvcar na label
     private void setErrorMessages(Map<String, String> error){
         Set<String> fields = error.keySet();
@@ -150,5 +212,17 @@ public class SellerFormController implements Initializable {
         for(DataChangeListener listener : dataChangeListeners){
             listener.onDataChanged();
         }
+    }
+    
+    private void initializeComboBoxDepartment() {
+        Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
+            @Override
+            protected void updateItem(Department item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? "" : item.getName());
+            }
+        };
+        comboBoxDepartment.setCellFactory(factory);
+        comboBoxDepartment.setButtonCell(factory.call(null));
     }
 }
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 /**
@@ -123,7 +124,8 @@ public class SellerListController implements Initializable, DataChangeListener {
             controller.setSeller(obj);
             
             //injetando dependencia
-            controller.setSellerService(new SellerService());
+            controller.setServices(new SellerService(), new DepartmentService());
+            controller.loadAssociatedObjects();
             
             //inscreve para o evento event
             controller.subscribeDataChangeListener(this);
@@ -139,6 +141,7 @@ public class SellerListController implements Initializable, DataChangeListener {
             dialogStage.initModality(Modality.WINDOW_MODAL);
             dialogStage.showAndWait();
         } catch (IOException e) {
+            e.printStackTrace();
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
         }
     }
